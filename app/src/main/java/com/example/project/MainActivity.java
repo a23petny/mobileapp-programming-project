6f30062292d9465d391ed492f8fff922ad297c76
a23petny <a23petny@student.his.se>
2024-05-13 08:52:38 +0200

    added the classes in the files form the netwroking uppgift

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -4,15 +4,82 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.security.PrivateKey;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@SuppressWarnings("FieldCanBeLocal")
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
+    private final String JSON_FILE = "mountains.json";
+
+    private ArrayList<Snack> snacks;
+    private ArrayList<RecyclerViewItem> items;
+    private RecyclerViewAdapter adapter;
+    @SuppressLint("WrongThread")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        snacks = new ArrayList<Snack>();
+
+        items = new ArrayList<>(Arrays.asList(
+                new RecyclerViewItem("Matterhorn"),
+                new RecyclerViewItem("Mont Blanc"),
+                new RecyclerViewItem("Denali")
+        ));
+
+        adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(RecyclerViewItem item) {
+                Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RecyclerView view = findViewById(R.id.recycler_view);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        view.setAdapter(adapter);
+
+
+
+        new JsonFile(this, this).execute(JSON_FILE);
+
+        //new JsonTask(this).execute(JSON_URL);
+
+
+        for (Snack snack : snacks) {
+            items.add(new RecyclerViewItem(snack.getName()));
+        }
+
+        adapter.notifyDataSetChanged();
     }
 
+
+    @Override
+    public void onPostExecute(String json) {
+        Log.d("MainActivity", "" + json);
+        Gson gson = new Gson();
+        // Unmarshall JSON -> list of objects
+        Type type = new TypeToken<List<Snack>>() {
+        }.getType();
+
+        snacks = gson.fromJson(json, type);
+    }
 }
