a1b8c1fe3dd6a60521d0a5d539a7b3ae0a9617dd
a23petny <a23petny@student.his.se>
2024-05-13 09:02:13 +0200

    1st version that works, added the correct class and xml file

--- app/src/main/java/com/example/project/Snack.java
+++ app/src/main/java/com/example/project/Snack.java
@@ -1,4 +1,15 @@
 package com.example.project;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Snack {
+        private String name;
+        @SerializedName("cost")
+        private int feet;
+        @SerializedName("size")
+        private int meter;
+
+        public String getName(){
+            return this.name;
+        }
 }
