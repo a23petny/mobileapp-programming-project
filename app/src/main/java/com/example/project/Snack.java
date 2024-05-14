3654b9ebfe0d49b5d82ff0d12c09eac00857b68f
a23petny <a23petny@student.his.se>
2024-05-14 10:49:31 +0200

    Added internet permit tested to add a image and increase the font size added some extra data fileds in snacks that i pretend to use

--- app/src/main/java/com/example/project/Snack.java
+++ app/src/main/java/com/example/project/Snack.java
@@ -2,12 +2,22 @@ package com.example.project;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.lang.ref.SoftReference;
+
 public class Snack {
+
+        private String image;
         private String name;
+        private char emoji;
+        private String healthLvl;
         @SerializedName("cost")
-        private int feet;
+        private int price;
         @SerializedName("size")
-        private int meter;
+        private int eta;
+
+        private String location;
+        private String ingredients;
+        private String desc;
 
         public String getName(){
             return this.name;
